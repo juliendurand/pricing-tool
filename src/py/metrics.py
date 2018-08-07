f184fe321fdcddb36ea00cb03c61981f55e5ca5d
Julien Durand <julien.durand@axa.fr>
2018-08-07 12:04:53 +0200

    clean metrics.py

--- src/py/metrics.py
+++ src/py/metrics.py
@@ -8,7 +8,6 @@ where ``**kwargs`` are **named** extra-parameters.
 """
 
 import numpy as np
-from scipy import interpolate
 
 
 def mean_absolute_error(y, y_hat, exposure):
@@ -21,7 +20,7 @@ def root_mean_square_error(y, y_hat, exposure):
 
 def check_deviance(y, y_pred, weight=None):
     """
-    Robust checks to run at beginning of deviance
+    checks to run at beginning of deviance
     """
     if isinstance(y_pred, (np.floating, float)):
         y_pred = np.repeat(y_pred, y.shape[0])
@@ -31,6 +30,94 @@ def check_deviance(y, y_pred, weight=None):
         assert weight.shape[0] == y.shape[0], \
             "weight and y do not have same shape"
     return y_pred
+
+
+def gaussian_deviance(y, y_pred, weight=None):
+    """
+    Deviance function for the gaussian/least squares model.
+
+    Parameters
+    ----------
+
+    y : ndarray
+        array containing the TRUE response
+    y_pred : ndarray
+        array containing the value predicted by the model
+    weight : ndarray, optional
+        array containing the weight (default 1)
+
+    Returns
+    -------
+    ndarray
+        computed deviance
+
+    """
+    y_pred = check_deviance(y, y_pred, weight=weight)
+    deviance_vector = np.square(y - y_pred)
+    if weight is not None:
+        deviance_vector = np.dot(weight, deviance_vector)
+    return 0.5 * np.sum(deviance_vector)
+
+
+def poisson_deviance(y, y_pred, weight=None):
+    """
+    Deviance function for the poisson model.
+
+    Parameters
+    ----------
+
+    y : ndarray
+        array containing the TRUE response
+    y_pred : ndarray
+        array containing the value predicted by the model
+    weight : ndarray, optional
+        array containing the weight (default 1)
+
+    Returns
+    -------
+    ndarray
+        computed deviance
+
+    """
+    y_pred = check_deviance(y, y_pred, weight=weight)
+    bool_zeros = y != 0
+    deviance_vector = np.zeros(y.shape[0])
+    deviance_vector[bool_zeros] = \
+        (y[bool_zeros] * np.log(y[bool_zeros] / y_pred[bool_zeros]) -
+            y[bool_zeros] + y_pred[bool_zeros])
+    deviance_vector[~bool_zeros] = - y[~bool_zeros] + y_pred[~bool_zeros]
+    if weight is not None:
+        deviance_vector = np.dot(weight, deviance_vector)
+
+    return 2 * np.sum(deviance_vector)
+
+
+def gamma_deviance(y, y_pred, weight=None):
+    """
+    Deviance function for gamma model.
+
+    Parameters
+    ----------
+
+    y : ndarray
+        array containing the TRUE response
+    y_pred : ndarray
+        array containing the value predicted by the model
+    weight : ndarray, optional
+        array containing the weight (default 1)
+
+    Returns
+    -------
+    ndarray
+        computed deviance
+
+    """
+    y_pred = check_deviance(y, y_pred, weight=weight)
+    deviance_vector = -np.log(y / y_pred) + (y - y_pred) / y_pred
+    if weight is not None:
+        deviance_vector = np.dot(weight, deviance_vector)
+
+    return 2 * np.sum(deviance_vector)
 
 
 def binomial_deviance(y, y_pred, weight=None):
@@ -60,97 +147,6 @@ def binomial_deviance(y, y_pred, weight=None):
     return 2 * np.sum(deviance_vector)
 
 
-def gamma_deviance(y, y_pred, weight=None):
-    """
-    Deviance function for gamma model.
-
-    Parameters
-    ----------
-
-    y : ndarray
-        array containing the TRUE response (either 0 or 1)
-    y_pred : ndarray
-        array containing the predicted probabilities by the model
-    weight : ndarray, optional
-        array containing the weight (default 1)
-
-    Returns
-    -------
-    ndarray
-        computed deviance
-
-
-    """
-    y_pred = check_deviance(y, y_pred, weight=weight)
-
-    deviance_vector = -np.log(y / y_pred) + (y - y_pred) / y_pred
-    if weight is not None:
-        deviance_vector = np.dot(weight, deviance_vector)
-
-    return 2 * np.sum(deviance_vector)
-
-
-def poisson_deviance(y, y_pred, weight=None):
-    """
-    Deviance function for the poisson model.
-
-    Parameters
-    ----------
-
-    y : ndarray
-        array containing the TRUE response (either 0 or 1)
-    y_pred : ndarray
-        array containing the predicted probabilities by the model
-    weight : ndarray, optional
-        array containing the weight (default 1)
-
-    Returns
-    -------
-    ndarray
-        computed deviance
-
-    """
-    y_pred = check_deviance(y, y_pred, weight=weight)
-
-    bool_zeros = y != 0
-    deviance_vector = np.zeros(y.shape[0])
-    deviance_vector[bool_zeros] = \
-        (y[bool_zeros] * np.log(y[bool_zeros] / y_pred[bool_zeros]) -
-            y[bool_zeros] + y_pred[bool_zeros])
-    deviance_vector[~bool_zeros] = - y[~bool_zeros] + y_pred[~bool_zeros]
-    if weight is not None:
-        deviance_vector = np.dot(weight, deviance_vector)
-
-    return 2 * np.sum(deviance_vector)
-
-
-def gaussian_deviance(y, y_pred, weight=None):
-    """
-    Deviance function for the gaussian/least squares model.
-
-    Parameters
-    ----------
-
-    y : ndarray
-        array containing the TRUE response (either 0 or 1)
-    y_pred : ndarray
-        array containing the predicted probabilities by the model
-    weight : ndarray, optional
-        array containing the weight (default 1)
-
-    Returns
-    -------
-    ndarray
-        computed deviance
-
-    """
-    y_pred = check_deviance(y, y_pred, weight=weight)
-    deviance_vector = np.square(y - y_pred)
-    if weight is not None:
-        deviance_vector = np.dot(weight, deviance_vector)
-    return 0.5 * np.sum(deviance_vector)
-
-
 def gaussian_pseudo_r2(y, y_pred, weight=None):
     """
     PseudoR2 for a Normal model.
@@ -159,9 +155,9 @@ def gaussian_pseudo_r2(y, y_pred, weight=None):
     ----------
 
     y : ndarray
-        array containing the TRUE response (either 0 or 1)
+        array containing the TRUE response
     y_pred : ndarray
-        array containing the predicted probabilities by the model
+        array containing the value predicted by the model
     weight : ndarray, optional
         array containing the weight (default 1)
 
@@ -190,9 +186,9 @@ def poisson_pseudo_r2(y, y_pred, weight=None):
     ----------
 
     y : ndarray
-        array containing the TRUE response (either 0 or 1)
+        array containing the TRUE response
     y_pred : ndarray
-        array containing the predicted probabilities by the model
+        array containing the value predicted by the model
     weight : ndarray, optional
         array containing the weight (default 1)
 
@@ -217,13 +213,14 @@ def gamma_pseudo_r2(y, y_pred, weight=None):
     """
     Pseudo R2 for gamma model
 
+
     Parameters
     ----------
 
     y : ndarray
-        array containing the TRUE response (either 0 or 1)
+        array containing the TRUE response
     y_pred : ndarray
-        array containing the predicted probabilities by the model
+        array containing the value predicted by the model
     weight : ndarray, optional
         array containing the weight (default 1)
 
@@ -276,8 +273,7 @@ def binomial_pseudo_r2(y, y_pred, weight=None):
                 np.average(y, weights=weight), weight))
 
 
-def area_lorentz_fast(y, y_pred, weight=None, resolution=5000,
-                      interpolation="constant", plot=False):
+def area_lorentz_fast(y, y_pred, weight=None):
     '''
     Calcultate the weighted gini.
 
@@ -285,36 +281,19 @@ def area_lorentz_fast(y, y_pred, weight=None, resolution=5000,
     ----------
 
     y : ndarray
-        array containing the TRUE response (either 0 or 1)
+        array containing the TRUE response
     y_pred : ndarray
-        array containing the predicted probabilities by the model
+        array containing the value predicted by the model
     weight : ndarray, optional
         array containing the weight (default 1)
-    resolution : int, optional
-        the number of points in the plot of the lorenz curve (default is 5000)
-    interpolation: {'linear', 'constant'}, optional
-        type of interpolation for the lorenz curve (default "constant")
-    plot: bool, optional
-        compute or not interpolation of lorenz curve(default False)
-
     '''
-    # Comments
-    # --------
-    #
-    # constant piecewise interpolation is useful when the number of observed
-    # ones is little (to underline the breakpoints),
-    #
-    # linear has nicer smoothing properties
-
-    if interpolation not in ["linear", "constant"]:
-        raise NotImplementedError("Interpolation requested not available.")
     if y.shape[0] != y_pred.shape[0]:
         raise ValueError("y and y_pred must have the same length.")
 
     n_samples = y.shape[0]
 
     if weight is None:
-        weight = np.repeat([1. / n_samples], n_samples)
+        weight = np.ones(n_samples)
 
     # Id of each column
     obs_col, pred_col, w_col, rank_col = 0, 1, 2, 3
@@ -322,7 +301,7 @@ def area_lorentz_fast(y, y_pred, weight=None, resolution=5000,
     # Order data following prediction
     ordered_data = np.column_stack((y, y_pred, weight, np.zeros(y.shape[0])))
 
-    pred_order = np.argsort(y_pred)[::-1]
+    pred_order = np.argsort(ordered_data[:, pred_col])[::-1]
     ordered_data = ordered_data[pred_order, :]
 
     # Compute the rank
@@ -336,49 +315,18 @@ def area_lorentz_fast(y, y_pred, weight=None, resolution=5000,
     intermediate = ordered_data[:, 0] * ordered_data[:, 2] * ordered_data[:, 3]
     rank_obs_sum = intermediate.sum()
 
-    # Compute the Gini coefficient
+    # Compute the weighted Gini
     gini = 1 - (2 / (total_weight * obs_sum)) * rank_obs_sum
-    # until here, as in the old code
-    if plot:
-        # Determine the points to plot
-        x_list = np.cumsum(ordered_data[:, w_col]) / total_weight
-        y_list = np.cumsum(ordered_data[:, w_col] * ordered_data[:, obs_col]) \
-            / obs_sum
-        x_interpolate = np.linspace(0, 1, num=resolution)
 
-        #    this is for linear interpolation
-        if interpolation == "linear":
-            y_interpolate = np.interp(x_interpolate, x_list, y_list)
-        elif interpolation == "constant":
-            # this is for piecewise interpolation
-            # (better when 1s and 0s are little)
-            f = interpolate.interp1d(x_list, y_list, kind='zero',
-                                     bounds_error=False)
-            y_interpolate = f(x_interpolate)
-
-            # manually make 0 and 1s outside the range (bug in interp1d)
-            y_interpolate[x_interpolate <= np.min(x_list)] = 0
-            y_interpolate[x_interpolate >= np.max(x_list)] = 1
-
-    # TODO: PLOT !
-
-        return gini, y_interpolate
-    else:
-        return gini
+    return gini
 
 
-def gini(y, y_pred, weights=None, normalize_gini=False, verbose=False):
+def gini(y, y_pred, weights=None, normalize_gini=False):
     # compute the weighted Gini
     gini = area_lorentz_fast(y, y_pred, weights)
-    if verbose:
-        print("Gini coefficient for prediction without normalization:", gini)
     if normalize_gini:
         # compute the weighted Gini for the "perfect model"
         gini_perfect_model = area_lorentz_fast(y, y, weights)
         # normalization
         gini = gini / gini_perfect_model
-        if verbose:
-            print("Gini coefficient of 'perfect' model:", gini_perfect_model)
-            print("The Gini coefficient for prediction after normalization :",
-                  gini)
     return gini
